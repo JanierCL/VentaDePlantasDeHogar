a09dfc83b704241936e4f41ed77503cb2c2c5ac4
JanierCL <140358835+JanierCL@users.noreply.github.com>
2023-08-27 23:31:38 -0500

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Planta from './Planta'; // Importa el componente
 
-test('renderiza el componente de planta', () => {
-  const { getByText } = render(<Planta />);
-  const titulo = getByText(/Planta Tropical/i);
-  expect(titulo).toBeInTheDocument();
+//no logré importar los componentes
+
+test('el título de la página es correcto', () => {
+  render(<App />);
+  const pageTitle = document.title;
+  expect(pageTitle).toBe('Venta de Plantas de Hogar');
+});
+
+test('los enlaces del menú de navegación son correctos', () => {
+  const navLinks = document.querySelectorAll('nav ul li a');
+  const expectedLinks = ['Inicio', 'Catálogo', 'Contacto'];
+  
+  navLinks.forEach((link, index) => {
+    expect(link.textContent).toBe(expectedLinks[index]);
+  });
+});
+
+test('el botón "Comprar" agrega elementos al carrito', () => {
+  const plantButtons = document.querySelectorAll('.plant button');
+  const carritoLista = document.getElementById('carrito-lista');
+  
+  plantButtons[0].click(); // Simula el clic en el primer botón "Comprar"
+  expect(carritoLista.children.length).toBe(1);
+});
+
+test('el pie de página contiene el año actual', () => {
+  const currentYear = new Date().getFullYear();
+  const footer = document.querySelector('footer');
+  expect(footer.textContent).toContain(currentYear);
 });
