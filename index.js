62bc6fd244ae935819d8810739084787c841b2ac
JanierCL <140358835+JanierCL@users.noreply.github.com>
2023-08-27 22:34:41 -0500

    Update index.js
    ajustes

--- index.js
+++ index.js
@@ -8,43 +8,6 @@ function cambiarPerfil() {
 }
 
 
-// Función para obtener detalles de la planta por imagen
-async function obtenerDetallesDePlantaPorImagen(C:\Users\JANIER\ventaplantas\src) {
-    const apiKey = 'Pkp22tJmOJku4cMAwq60VynFCn34H15heO8QQOBuwJArCLUgsH'; // se ingresa clave de API
-    const apiUrl = `https://plant.id/api/v3/identify?images=${C:\Users\JANIER\ventaplantas\src\imagenes\planta2}`;
-
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'Api-Key': apiKey
-            }
-        });
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error al obtener detalles de la planta:', error);
-        return null;
-    }
-}
-
-// Función para manejar la identificación de planta por imagen
-async function identificarPlantaPorImagen(C:\Users\JANIER\ventaplantas\src\imagenes\planta2) {
-    const detallesDePlanta = await obtenerDetallesDePlantaPorImagen(C:\Users\JANIER\ventaplantas\src\imagenes\planta2);
-
-    if (detallesDePlanta && detallesDePlanta.suggestions.length > 0) {
-        const plantaIdentificada = detallesDePlanta.suggestions[0].plant_name;
-        const plantaElement = document.getElementById('planta-identificada');
-        plantaElement.textContent = `Planta identificada: ${plantaIdentificada}`;
-    } else {
-        const plantaElement = document.getElementById('planta-identificada');
-        plantaElement.textContent = 'No se pudo identificar la planta.';
-    }
-}
-
-
-
 // Código del carrito de compras y el inventario
 const inventario = [
     { id: 1, nombre: 'Planta Tropical', precio: 20000, stock: 0 },
@@ -94,4 +57,40 @@ function actualizarCarrito() {
 function mostrarCarrito() {
     const carritoElement = document.getElementById('carrito');
     carritoElement.style.display = 'block';
+}
+
+
+// Función para obtener detalles de la planta por imagen
+async function obtenerDetallesDePlantaPorImagen(C:\Users\JANIER\ventaplantas\src) {
+    const apiKey = 'Pkp22tJmOJku4cMAwq60VynFCn34H15heO8QQOBuwJArCLUgsH'; // se ingresa clave de API
+    const apiUrl = `https://plant.id/api/v3/identify?images=${C:\Users\JANIER\ventaplantas\src\imagenes\planta2}`;
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Api-Key': apiKey
+            }
+        });
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error al obtener detalles de la planta:', error);
+        return null;
+    }
+}
+
+// Función para manejar la identificación de planta por imagen
+async function identificarPlantaPorImagen(C:\Users\JANIER\ventaplantas\src\imagenes\planta2) {
+    const detallesDePlanta = await obtenerDetallesDePlantaPorImagen(C:\Users\JANIER\ventaplantas\src\imagenes\planta2);
+
+    if (detallesDePlanta && detallesDePlanta.suggestions.length > 0) {
+        const plantaIdentificada = detallesDePlanta.suggestions[0].plant_name;
+        const plantaElement = document.getElementById('planta-identificada');
+        plantaElement.textContent = `Planta identificada: ${plantaIdentificada}`;
+    } else {
+        const plantaElement = document.getElementById('planta-identificada');
+        plantaElement.textContent = 'No se pudo identificar la planta.';
+    }
 }
